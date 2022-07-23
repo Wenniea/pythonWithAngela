958c3a52f726720e80d0c439afeabf923a7a9539
Wenniea <wenjingj1231@gmail.com>
2022-07-23 16:38:54 -0500

    revision after the video

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -29,8 +29,55 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+def isSufficient(drink):
+    """If sufficient resources, then return True, otherwise False"""
+    for k, v in drink['ingredients'].items():
+        if resources[k] - v < 0:
+            print('Sorry there is not enough ' + k + ' .')
+            return False
+        else:
+            return True
 
-# prompt user
+def processCoins():
+    """ caluculate the amount of coins inserted"""
+    print('Please insert coins.')
+    print('How many quarters?: ')
+    quarters = input()
+    print('How many dimes?: ')
+    dimes = input()
+    print('How many nickles?: ')
+    nickles = input()
+    print('How many pennies?: ')
+    pennies = input()
+
+    moneyInserted = (int(quarters) * 0.25) + (int(dimes) * 0.10) + (int(nickles) * 0.05) + (
+            int(pennies) * 0.01)
+
+    return moneyInserted
+
+def enoughMoney(moneyInserted, drink):
+    """ if enough money inserted then return True, else return False"""
+    itemCost = drink['cost']
+    global money
+    if moneyInserted < itemCost:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    elif moneyInserted > itemCost:
+        change = moneyInserted - itemCost
+        print('Here is $ {:0.2f} dollars in change.'.format(change))  # round to two decimal
+        # place
+        money += itemCost
+        return True
+    elif moneyInserted == itemCost:
+        money += itemCost
+        return True
+
+def makeCoffee(drink):
+    """ deduct the ingredients from the coffee machine resource"""
+    for k, v in drink['ingredients'].items():
+        resources[k] = resources[k] - v
+    print('Here is your ' + choice + ' ☕️. Enjoy!')
+
 machineOn = True
 money = 0.0
 while machineOn:
@@ -39,49 +86,15 @@ while machineOn:
     if choice == 'off':
         machineOn = False
         break
-    if choice == 'report':
+    elif choice == 'report':
         print('Water: ' + str(resources['water']) + 'ml')
         print('Milk: ' + str(resources['milk']) + 'ml')
         print('Coffee: ' + str(resources['coffee']) + 'g')
         print('Money: $' + str(money))
-        continue
+    else:
+        drink = MENU[choice]
+        if isSufficient(drink):
+            moneyInserted = processCoins()
+            if enoughMoney(moneyInserted, drink):
+                makeCoffee(drink)
 
-    enough = True
-
-    if enough:
-        # check resources sufficient
-        for k, v in MENU[choice]['ingredients'].items():
-            if resources[k] - v < 0:
-                print('Sorry there is not enough ' + k + ' .')
-                enough = False
-                break
-
-    if enough:
-        # process coins
-        print('Please insert coins.')
-        print('How many quarters?: ')
-        quarters = input()
-        print('How many dimes?: ')
-        dimes = input()
-        print('How many nickles?: ')
-        nickles = input()
-        print('How many pennies?: ')
-        pennies = input()
-
-        moneyInserted = (int(quarters) * 0.25) + (int(dimes) * 0.10) + (int(nickles) * 0.05) + (
-                int(pennies) * 0.01)
-
-        itemCost = MENU[choice]['cost']
-        if moneyInserted < itemCost:
-            print("Sorry that's not enough money. Money refunded.")
-            enough = False
-        elif moneyInserted > itemCost:
-            change = moneyInserted - itemCost
-            print('Here is $ {:0.2f} dollars in change.'.format(change))  # round to two decimal
-            # place
-
-        if enough:
-            for k, v in MENU[choice]['ingredients'].items():
-                resources[k] = resources[k] - v
-            money += itemCost
-            print('Here is your ' + choice + ' ☕️. Enjoy!')
