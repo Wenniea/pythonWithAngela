a55396e14a06e519bd2a20f672fe392d5126f2fa
Wenniea <wenjingj1231@gmail.com>
2022-08-03 18:17:46 -0500

    OOP Coffee Machine

--- day-16-start.py
+++ day-16-start.py
@@ -1,10 +1,18 @@
 import turtle
 
-from turtle import Turtle, Screen
-timmy = Turtle()  #created an object timmy from the turtle class
-timmy.shape('turtle')
-timmy.color('purple')
-timmy.forward(100)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# from turtle import Turtle, Screen
+# timmy = Turtle()  #created an object timmy from the turtle class
+# timmy.shape('turtle')
+# timmy.color('purple')
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmandar"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
