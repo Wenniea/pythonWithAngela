82cd626977622536800090fdbc0740cc5e352ce7
Wenniea <wenjingj1231@gmail.com>
2022-12-20 11:32:27 -0600

    finish part 1 of day 17

--- day17/day-17-start.py
+++ day17/day-17-start.py
@@ -1,15 +1,26 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username;
+        self.followers = 0; # default value
+        self.following = 0
 
+    def follow(self, user): # method always need a self parameter
+        user.followers += 1
+        self.following += 1
 
-user_1 = User()
-user_1.id = '001'  # attributes
-user_1.username = 'angela'
+user_1 = User("001", "angela") # this is way more organize than the one below
+user_2 = User("002", "jack")
 
 print(user_1.username)
+print(user_1.followers)
 
-user_2 = User()
-user_2.id = '002'
-user_2.username = 'jack'
+#user_1 = User()
+#user_1.id = '001'
+#user_1.username = 'angela'
 
-print(user_2.username)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
