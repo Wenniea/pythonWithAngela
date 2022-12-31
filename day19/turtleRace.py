c77bd42de06c16922646231cfec4b4079f20fb53
Wenniea <wenjingj1231@gmail.com>
2022-12-30 23:13:20 -0600

    started the turtle race

---
+++ day19/turtleRace.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a "
+                                                          "color: ")
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+# tim = Turtle(shape='turtle')
+# tim.penup()
+# tim.goto(x=-200, y=-100)
+
+for i in range(6):
+    tim = Turtle(shape='turtle')
+    tim.color(colors[i])
+    tim.penup()
+    tim.goto(x=-200, y=130 - 50 * i)
+
+screen.exitonclick()
